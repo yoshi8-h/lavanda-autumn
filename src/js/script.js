c2a1d800645387b0db632ddd99b4034faccce1b0
yoshi8-h <hirao.y.freelance@gmail.com>
2024-08-11 12:16:04 +0900

    s4-sp セクション完成時

--- src/js/script.js
+++ src/js/script.js
@@ -3,3 +3,20 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
 
 
 });
+
+/* -------------------------------------------------------------------------------- */
+/* swiper (スワイパー) s4-sp セクション */
+// swiperの定義
+const s4SpSwiper = new Swiper("#js-s4-sp-swiper", {
+  loop: true,
+
+  slidesPerView: 'auto', // スライドの幅をCSSで指定
+  spaceBetween: 5,
+  autoplay: {
+    delay: 2300,  // 次のスライドに切り替わるまでの時間
+    disableOnInteraction: false,  // ユーザーがドラッグなどの操作をしても自動再生が止まらないように。
+  },
+  speed: 900,  // 切り替わる最中のスピード(ミリ秒)
+});
+
+
